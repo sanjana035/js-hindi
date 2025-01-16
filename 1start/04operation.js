1a174df96fb316e2952169d8fcf7b1d5ecb8d64f
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-16 08:14:13 +0000

    operation

--- 1start/04operation.js
+++ 1start/04operation.js
@@ -1,10 +1,25 @@
 let str1 = "hello"
 let str2 = " sanjana"
 let str3 = str1 + str2
-console.log(str3);
+// console.log(str3);
 
-console.log("2" + 3);
-console.log(2 + "3");
-console.log("2" + 3 + 3);
-console.log(2 + 3 + "3");
-console.log(+true);
+// console.log("2" + 3);
+// console.log(2 + "3");
+// console.log("2" + 3 + 3);
+// console.log(2 + 3 + "3");
+// console.log(+true);
+
+// prefix or postfix conversion
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
