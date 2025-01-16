69014b08d341a5e19a43d1cbe5b64e79fa2f5174
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-16 07:45:54 +0000

    dataconversion

---
+++ 1start/03conversion.js
@@ -0,0 +1,16 @@
+let score = 23
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let convert = String(score);
+console.log(typeof (convert));
+
+
+// "33" = 33
+// "33abac" = NaN
+//true = 1
+//NUll = 0
+// "" = false
+// "sanjana" = true
+
