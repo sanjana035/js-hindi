f30b64952b4543db969827512f99dc57b5d234e5
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-16 08:08:10 +0000

    operation

---
+++ 1start/04operation.js
@@ -0,0 +1,10 @@
+let str1 = "hello"
+let str2 = " sanjana"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("2" + 3);
+console.log(2 + "3");
+console.log("2" + 3 + 3);
+console.log(2 + 3 + "3");
+console.log(+true);
