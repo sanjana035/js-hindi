6ab724cc0833a73aca87b213d113267024397afe
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-16 20:26:18 +0000

    summaryDataTypes

---
+++ 1start/05dataTypesSummary.js
@@ -0,0 +1,24 @@
+// primitive DAta Type
+/* 7 types : string
+            Number
+            boolean
+            Null
+            Undefined
+            Symbol
+            BigInt
+*/
+// Reference (Non Primitive)
+/*
+        Array
+        Object
+        Functions
+*/
+const myArr = ["sanjana","satwik","sadhvi"];
+const myObj = {
+        name : "sanjana",
+        age :  25,
+}
+
+const myFun = function(){
+        console.log("hello World");
+}
