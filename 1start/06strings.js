27e8e358b2270d7b110cbc75a1beb90413edadd3
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-17 21:25:37 +0000

    strings

---
+++ 1start/06strings.js
@@ -0,0 +1,29 @@
+const name = "sanjana"
+const count = 24
+
+
+console.log(name +  count + " value");
+console.log(`hello my name is ${name} and my age is ${count}`);
+
+
+
+const gameName = new String("muskan");
+// console.log(gameName);
+// console.log(gameName.charAt('0'));
+// console.log(gameName.indexOf('k'));
+// console.log(gameName.toUpperCase());
+
+// const newString = gameName.substring(0,5);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-6,4);
+// console.log(anotherString);
+
+// const string2 = "    chaurasiya  "
+// console.log(string2);
+// console.log(string2.trim());
+
+const url = "https://sanjana.com/sanjana%20Chaurasiyaa"
+console.log(url.replace('%20','-'));
+console.log(url.includes('satna'));
+console.log(gameName.split('-'));
