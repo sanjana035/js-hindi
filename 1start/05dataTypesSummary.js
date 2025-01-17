0836fe8148abb30d05275b26008155f6257aa222
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-17 20:52:09 +0000

    summuryDataTypes

--- 1start/05dataTypesSummary.js
+++ 1start/05dataTypesSummary.js
@@ -22,3 +22,38 @@ const myObj = {
 const myFun = function(){
         console.log("hello World");
 }
+
+
+
+
+/// Memory Type
+//Stack(primitive) - have a copy of any created variable 
+//Heap(Non Primitive) - (refernce) means koi bhi chnge karege to original me bhi change hoga
+
+//ex-stack
+
+let firstName = "sanjana"
+let name2 = firstName
+
+name2 = "chaurasiya"
+console.log(firstName);
+console.log(name2);
+
+
+//ex-Heap
+
+let user1 = {
+        email : "sanjana@gmail.com",
+        age: 23,
+}
+let user2 = user1;
+
+user2.email = "Chaurasiya@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
+
+
+
+
+
