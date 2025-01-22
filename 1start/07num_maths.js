f5e5a8e5086b62d8e96525060eabfd6340ac5390
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-22 17:36:11 +0000

    math and number

---
+++ 1start/07num_maths.js
@@ -0,0 +1,20 @@
+const score = 400
+console.log(score);
+
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+
+console.log(Math.round(33.7));
+console.log(Math.min(4,3,2,5));
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
