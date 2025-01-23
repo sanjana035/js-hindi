bf03ddeb9e8b8456c177ef5fa3dda0978d586211
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-01-23 10:02:29 +0000

    Date and Time

---
+++ 1start/08dateAndTime.js
@@ -0,0 +1,7 @@
+//Dates
+//date is called an object in javaScript
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.getMonth()+1);
