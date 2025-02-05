91b705af7f76d790703fb787451a400d6a76504a
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-02-05 17:49:56 +0000

    scope

---
+++ 3basics/04scope.js
@@ -0,0 +1,27 @@
+function one() {
+    const username = "sanjana"
+    function two() {
+        const website = "youTube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+one();
+
+//++++++++++++++++++++interesting++++++++++++++++++++
+// function addOne(num) {
+//     return num+1;
+// }
+// addOne(3);
+/////this is basic function
+//isme function bad me likh pehle call kr skate hai
+
+// const addtwo = function (num) {
+//     return num +2
+// }
+// addtwo(5)
+
+// this is also a function but it is known as expression
+//isme function declaration ke pehle access nhi kar pate
+
