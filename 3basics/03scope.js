2c5d5eaddebc6d7ce9e84902db8bec96f20695ab
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-02-05 17:34:27 +0000

    scope

---
+++ 3basics/03scope.js
@@ -0,0 +1,27 @@
+// let a = 10;
+
+// if(true){
+//     let a = 100;
+//     const b = 20;
+//     console.log("Inner: ",a);
+    
+// }
+// console.log(a);
+
+////// { } curly braces called scope
+
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var c = 3000
+// if(true){
+//     let a = 10
+//     var c = 30
+// }
+// console.log(c);
