bd6a170a654fd0ad4ad248219f0f2d0a12a0dcea
Sanjana Chaurasiya <159447806+sanjana035@users.noreply.github.com>
2025-03-30 20:23:33 +0000

    one

--- 5loopsIterations/one.js
+++ 5loopsIterations/one.js
@@ -3,11 +3,11 @@
 //     console.log(element);
 // }
 
-// let myArray = ["sanjana","muskan","drishti"];
-// for(let i = 0; i < myArray.length; i++){
-//     const element = myArray[i];
-//     console.log(element);
-// }
+let myArray = ["sanjana","muskan","drishti"];
+for(let i = 0; i < myArray.length; i++){
+    const element = myArray[i];
+    console.log(element);
+}
 
 // for(let i = 1; i <= 10; i++){
 //     console.log(`table of ${i}`);    
@@ -16,12 +16,12 @@
 //     }
 // }
 
-for (let index = 1; index <= 10; index++) {
-    const element = index * 2;
-    if (element == 10){
-        console.log(`found the ${element}`);
-        continue;
-    }   
-    console.log(element); 
-}
+// for (let index = 1; index <= 10; index++) {
+//     const element = index * 2;
+//     if (element == 10){
+//         console.log(`found the ${element}`);
+//         continue;
+//     }   
+//     console.log(element); 
+// }
 //break and continue
